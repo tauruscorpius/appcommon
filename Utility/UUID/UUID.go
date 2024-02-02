bab3d90672384e580bb76f102a976fe3f3eb0d05
tauruscorpius <tauruscorpiuss@gmail.com>
2024-02-02 11:52:29 +0800

    add utility/uuid

---
+++ Utility/UUID/UUID.go
@@ -0,0 +1,63 @@
+package UUID
+
+import (
+	"encoding/binary"
+	"github.com/oklog/ulid"
+	uuid "github.com/satori/go.uuid"
+	"math/rand"
+	"os"
+	"sync"
+	"sync/atomic"
+	"time"
+)
+
+func _uuid() string {
+	return uuid.NewV4().String()
+}
+
+var processPid = uint16(os.Getpid())
+
+type notReallyEntropy uint64
+
+func (u *notReallyEntropy) Read(p []byte) (int, error) {
+	v := atomic.AddUint64((*uint64)(u), 1)
+	l := len(p)
+	if l >= 10 {
+		binary.BigEndian.PutUint16(p[l-10:], processPid)
+		binary.BigEndian.PutUint64(p[l-10+2:], v)
+	} else if l >= 8 {
+		binary.BigEndian.PutUint64(p[l-8:], v)
+	} else if l >= 4 {
+		binary.BigEndian.PutUint32(p[l-4:], uint32(v))
+	} else if l >= 2 {
+		binary.BigEndian.PutUint16(p[l-4:], uint16(v))
+	} else if l >= 0 {
+		p[0] = uint8(v)
+	}
+	return len(p), nil
+}
+
+var uidRW sync.RWMutex
+var entropyForULID = notReallyEntropy(rand.New(rand.New(rand.NewSource(time.Now().UnixNano()))).Uint64())
+
+// _ulid -> 128 bits
+// 48bits (16+32) timestamp
+// 80bits (16+32+32) random value
+func _ulid() (string, error) {
+	uidRW.Lock()
+	defer uidRW.Unlock()
+	x, er := ulid.New(ulid.Timestamp(time.Now()), &entropyForULID)
+	return x.String(), er
+}
+
+func GetUid() string {
+	id, err := GetUniqLexId()
+	if err != nil {
+		return _uuid()
+	}
+	return id
+}
+
+func GetUniqLexId() (string, error) {
+	return _ulid()
+}
--- go.mod
+++ go.mod
@@ -15,6 +15,7 @@ require (
 	github.com/dgryski/go-rendezvous v0.0.0-20200823014737-9f7001d12a5f // indirect
 	github.com/modern-go/concurrent v0.0.0-20180228061459-e0a39a4cb421 // indirect
 	github.com/modern-go/reflect2 v1.0.2 // indirect
+	github.com/oklog/ulid v1.3.1 // indirect
 	github.com/sirupsen/logrus v1.9.2 // indirect
 	github.com/stretchr/testify v1.7.0 // indirect
 	golang.org/x/sys v0.8.0 // indirect
