05b1c223d1e4356f4616866a8097d7194a2980c6
tauruscorpius <tauruscorpiuss@gmail.com>
2025-04-25 14:50:29 +0800

    Caching asynchronous log writer

--- Log/LogImpl.go
+++ Log/LogImpl.go
@@ -1,6 +1,7 @@
 package Log
 
 import (
+	"errors"
 	"fmt"
 	"github.com/tauruscorpius/appcommon/Consts"
 	"os"
@@ -15,64 +16,175 @@ const (
 )
 
 var (
-	fileLog     *os.File = nil
-	keepLogDays          = 30
+	keepLogDays = 30
 	logKey      string
-	logMutex    sync.RWMutex
 )
 
-func LogCreator(logKey string) {
-	logMutex.Lock()
-	defer logMutex.Unlock()
-	logHome := os.Getenv("HOME") + "/log"
-	_ = os.MkdirAll(logHome, 0777)
-	timeNow := time.Now()
-	newFile := func(f string) *os.File {
-		fh, err := os.OpenFile(f, os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0666)
-		if err != nil {
-			fmt.Printf("Create Log File %s Error : %v, Use Stdout innstead of\n", f, err)
-			return os.Stdout
-		}
-		return fh
+var bufferLogWriter *BufferedLogWriter = nil
+
+type BufferedLogWriter struct {
+	logMutex    sync.Mutex
+	logDir      string
+	logPrefix   string
+	fileName    string
+	bufferMutex sync.RWMutex
+	buffer      strings.Builder
+	chanFlush   chan struct{}
+	chanClose   chan struct{}
+	fileHandle  *os.File
+}
+
+func CreateBufferedLogWriter(logKey string) *BufferedLogWriter {
+	logDir := os.Getenv("HOME") + string(os.PathSeparator) + "log"
+	logPrefix := logDir + string(os.PathSeparator) + logKey
+	return &BufferedLogWriter{
+		logDir:     logDir,
+		logPrefix:  logPrefix,
+		chanFlush:  make(chan struct{}, 512),
+		chanClose:  make(chan struct{}, 512),
+		fileHandle: nil,
 	}
-	fileName := logHome + "/" + logKey + timeNow.Format(Consts.DateDF) + logPathDelimiter + "log"
-	var closeLog *os.File = nil
-	st, err := os.Stat(fileName)
+}
+
+func (b *BufferedLogWriter) CheckLogDirExists(logKey string) bool {
+	err := os.MkdirAll(b.logDir, 0777)
 	if err != nil {
-		closeLog = fileLog
-		fileLog = nil
+		fmt.Printf("create log dir %s error : %v\n", b.logDir, err)
+		return false
+	}
+	return true
+}
+
+func (b *BufferedLogWriter) CheckLogFileRotation() string {
+	fileName := b.logPrefix + "_" + time.Now().Format(Consts.DateDF) + ".log"
+	st, err := os.Stat(fileName)
+	if b.fileHandle == nil || err != nil {
+		b.CreateLogFile(fileName)
+		prevFileName := b.fileName
+		b.fileName = fileName
+		return prevFileName
 	} else if st.IsDir() {
-		fmt.Printf("Log File Conflict with Dir Name : %s\n", fileName)
+		fmt.Printf("log file name conflict with dir name : " + fileName)
 	}
-	if fileLog == nil {
-		fh := newFile(fileName)
-		l.SetOutput(fh)
-		fileLog = fh
+	return ""
+}
+
+func (b *BufferedLogWriter) CreateLogFile(fileName string) {
+	b.Close()
+	fh, err := os.OpenFile(fileName, os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0666)
+	if err != nil {
+		fmt.Printf("create Log File %s Error : %v, use default stdout\n", fileName, err)
+		return
 	}
-	if closeLog != nil {
-		_ = closeLog.Close()
-		closeLog = nil
+	b.fileHandle = fh
+}
+
+const WriteLogFileSize = 1024 * 1024
+const MaxBufferedLogFileSize = 50 * 1024 * 1024
+
+func (b *BufferedLogWriter) writeCloseChan() {
+	b.writeLogChan(b.chanClose)
+}
+
+func (b *BufferedLogWriter) writeFlushChan() {
+	b.writeLogChan(b.chanFlush)
+}
+
+func (b *BufferedLogWriter) writeLogChan(ch chan struct{}) {
+	if len(ch) >= cap(ch) {
+		fmt.Printf("log writer chan full\n")
+		return
+	}
+	ch <- struct{}{}
+}
+
+func (b *BufferedLogWriter) Write(p []byte) (n int, err error) {
+	b.bufferMutex.Lock()
+	defer b.bufferMutex.Unlock()
+
+	if b.buffer.Len() >= MaxBufferedLogFileSize {
+		return 0, errors.New("buffer overflow")
+	}
+	a, e := b.buffer.Write(p)
+	if b.buffer.Len() >= WriteLogFileSize {
+		b.writeFlushChan()
+	}
+	return a, e
+}
+
+func (b *BufferedLogWriter) Close() {
+	b.logMutex.Lock()
+	defer b.logMutex.Unlock()
+
+	if b.fileHandle != nil {
+		b.fileHandle.Close()
+		b.fileHandle = nil
+	}
+}
+
+func (b *BufferedLogWriter) getBuffer() string {
+	b.bufferMutex.Lock()
+	defer b.bufferMutex.Unlock()
+	if b.buffer.Len() == 0 {
+		return ""
+	}
+	var s string
+	s += b.buffer.String()
+	b.buffer.Reset()
+	return s
+}
+
+func (b *BufferedLogWriter) flush() {
+	s := b.getBuffer()
+	if len(s) > 0 {
+		fileHandle := b.fileHandle
+		if fileHandle == nil {
+			fileHandle = os.Stdout
+		}
+		fileHandle.Write([]byte(s))
+		fileHandle.Sync()
+	}
+}
+
+func (b *BufferedLogWriter) autoFlush() {
+	ticker := time.NewTicker(1 * time.Second)
+	defer ticker.Stop()
+
+	for {
+		select {
+		case <-ticker.C:
+			b.flush()
+		case <-b.chanFlush:
+			b.flush()
+		case <-b.chanClose:
+			b.flush()
+			b.Close()
+			return
+		}
 	}
 }
 
 func SetOutput(logBaseName string) {
-	logKey := logBaseName
-	LogCreator(logKey)
+	logKey = logBaseName
+	bufferLogWriter = CreateBufferedLogWriter(logKey)
+	bufferLogWriter.CheckLogFileRotation()
+	l.SetOutput(bufferLogWriter)
+	go bufferLogWriter.autoFlush()
 	go func() {
 		for {
+			bufferLogWriter.CheckLogDirExists(logKey)
+			rotationFile := bufferLogWriter.CheckLogFileRotation()
+			if len(rotationFile) > 0 {
+				fmt.Printf("log file rotated : file : %s, new file : %s\n", rotationFile, bufferLogWriter.fileName)
+			}
 			time.Sleep(time.Second)
-			LogCreator(logKey)
 		}
 	}()
 }
 
 func CloseOutput() {
-	logMutex.Lock()
-	defer logMutex.Unlock()
-
-	if fileLog != nil {
-		fileLog.Close()
-		fileLog = nil
+	if bufferLogWriter != nil {
+		bufferLogWriter.writeCloseChan()
 	}
 }
 
--- Log/Log_test.go
+++ Log/Log_test.go
@@ -7,9 +7,10 @@ import (
 
 func TestLogOut(t *testing.T) {
 	SetOutput("test_LogOut")
-	time.Sleep(time.Second * 2)
 	for i := 0; i < 10; i++ {
 		Criticalf("test")
 		time.Sleep(time.Second)
 	}
+	CloseOutput()
+	time.Sleep(time.Second)
 }
