6dbb2a71d8aa8e378f3689ba6472623a72e473e2
tauruscorpius <tauruscorpiuss@gmail.com>
2025-06-19 11:35:48 +0800

    v9 go-redis

---
+++ RedisCluster/v9/RedisCluster.go
@@ -0,0 +1,78 @@
+package RedisCluster
+
+import (
+	"context"
+	"errors"
+	"github.com/redis/go-redis/v9"
+	"github.com/tauruscorpius/appcommon/Log"
+	"sync"
+	"time"
+)
+
+var (
+	redisBase *RedisClusterBase
+	onceSvc   sync.Once
+)
+
+func GetRedisBase() *RedisClusterBase {
+	onceSvc.Do(func() {
+		redisBase = &RedisClusterBase{}
+	})
+	return redisBase
+}
+
+type RedisClusterConfig struct {
+	redisUser     string
+	redisPassword string
+	pool          []string
+}
+
+func (t *RedisClusterConfig) Add(host, port string) {
+	t.pool = append(t.pool, host+":"+port)
+}
+
+func (t *RedisClusterConfig) SetAuth(user, password string) {
+	t.redisUser = user
+	t.redisPassword = password
+}
+
+type RedisClusterBase struct {
+	cluster *redis.ClusterClient
+}
+
+func (t RedisClusterBase) Get() *redis.ClusterClient {
+	return t.cluster
+}
+
+func (t *RedisClusterBase) ClusterInit(redisPool *RedisClusterConfig) error {
+	if redisPool == nil {
+		t.cluster = nil
+		return nil
+	}
+	Log.Criticalf("Redis Pool : %+v", *redisPool)
+	option := &redis.ClusterOptions{
+		Addrs:        redisPool.pool,
+		DialTimeout:  5 * time.Second,
+		ReadTimeout:  1 * time.Second,
+		WriteTimeout: 1 * time.Second,
+		PoolSize:     500,
+		PoolTimeout:  30 * time.Second,
+	}
+	if redisPool.redisPassword != "" {
+		option.Username = redisPool.redisUser
+		option.Password = redisPool.redisPassword
+	}
+	cluster := redis.NewClusterClient(option)
+	if cluster == nil {
+		return errors.New("cluster new")
+	}
+	ctx := context.Background()
+	ctxTimeout, _ := context.WithTimeout(ctx, time.Second)
+	pong, err := cluster.Ping(ctxTimeout).Result()
+	Log.Infof("redis pong status[%v]\n", pong)
+	if err != nil {
+		return err
+	}
+	t.cluster = cluster
+	return nil
+}
--- go.mod
+++ go.mod
@@ -5,17 +5,18 @@ go 1.20
 require (
 	github.com/go-redis/redis/v8 v8.11.5
 	github.com/json-iterator/go v1.1.12
+	github.com/oklog/ulid v1.3.1
+	github.com/redis/go-redis/v9 v9.10.0
 	github.com/satori/go.uuid v1.2.0
 	github.com/tauruscorpius/logrus v1.0.0
 	golang.org/x/net v0.10.0
 )
 
 require (
-	github.com/cespare/xxhash/v2 v2.1.2 // indirect
+	github.com/cespare/xxhash/v2 v2.3.0 // indirect
 	github.com/dgryski/go-rendezvous v0.0.0-20200823014737-9f7001d12a5f // indirect
 	github.com/modern-go/concurrent v0.0.0-20180228061459-e0a39a4cb421 // indirect
 	github.com/modern-go/reflect2 v1.0.2 // indirect
-	github.com/oklog/ulid v1.3.1 // indirect
 	github.com/sirupsen/logrus v1.9.2 // indirect
 	github.com/stretchr/testify v1.7.0 // indirect
 	golang.org/x/sys v0.8.0 // indirect
